1fa819737f8f5e9950dd3849e320789258b4dfea
Maxtro2002 <jspr200231@gmail.com>
2023-04-20 12:31:10 -0500

    Mejora en navBar inferior

--- lib/entryPiont.dart
+++ lib/entryPiont.dart
@@ -3,6 +3,9 @@ import 'package:rive/rive.dart';
 import 'package:rive_animation/constants.dart';
 import 'package:rive_animation/utils/riveUtils.dart';
 
+import 'components/animatedBar.dart';
+import 'models/riveAsset.dart';
+
 class EntryPoint extends StatefulWidget {
   const EntryPoint({super.key});
 
@@ -11,18 +14,17 @@ class EntryPoint extends StatefulWidget {
 }
 
 class _EntryPointState extends State<EntryPoint> {
-  
   RiveAsset selectedButtonNav = bottomNavs.first;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: SafeArea(
         child: Container(
-          padding: EdgeInsets.all(12),
-          margin: EdgeInsets.symmetric(horizontal: 24),
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 24),
           decoration: BoxDecoration(
             color: backgroundColor2.withOpacity(0.8),
-            borderRadius: BorderRadius.all(Radius.circular(24)),
+            borderRadius: const BorderRadius.all(Radius.circular(24)),
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -32,7 +34,7 @@ class _EntryPointState extends State<EntryPoint> {
                 (index) => GestureDetector(
                   onTap: () {
                     bottomNavs[index].input!.change(true);
-                    if (bottomNavs[index] != selectedButtonNav){
+                    if (bottomNavs[index] != selectedButtonNav) {
                       setState(() {
                         selectedButtonNav = bottomNavs[index];
                       });
@@ -41,24 +43,31 @@ class _EntryPointState extends State<EntryPoint> {
                       bottomNavs[index].input!.change(false);
                     });
                   },
-                  child: SizedBox(
-                    height: 36,
-                    width: 36,
-                    child: Opacity(
-                      opacity: bottomNavs[index] == selectedButtonNav ? 1 : 0.5,
-                      child: RiveAnimation.asset(
-                        bottomNavs.first.src,
-                        artboard: bottomNavs[index].artboard,
-                        onInit: (artboard) {
-                          StateMachineController controller =
-                              RiveUtils.getRiveController(artboard,
-                                  stateMachineName:
-                                      bottomNavs[index].stateMachineName);
-                          bottomNavs[index].input =
-                              controller.findSMI("active") as SMIBool;
-                        },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      AnimatedBar(isActive: bottomNavs[index] == selectedButtonNav),
+                      SizedBox(
+                        height: 36,
+                        width: 36,
+                        child: Opacity(
+                          opacity:
+                              bottomNavs[index] == selectedButtonNav ? 1 : 0.5,
+                          child: RiveAnimation.asset(
+                            bottomNavs.first.src,
+                            artboard: bottomNavs[index].artboard,
+                            onInit: (artboard) {
+                              StateMachineController controller =
+                                  RiveUtils.getRiveController(artboard,
+                                      stateMachineName:
+                                          bottomNavs[index].stateMachineName);
+                              bottomNavs[index].input =
+                                  controller.findSMI("active") as SMIBool;
+                            },
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
                 ),
               )
@@ -70,34 +79,3 @@ class _EntryPointState extends State<EntryPoint> {
   }
 }
 
-class RiveAsset {
-  final String artboard, stateMachineName, title, src;
-  late SMIBool? input;
-
-  RiveAsset(this.src,
-      {required this.artboard,
-      required this.stateMachineName,
-      required this.title,
-      this.input});
-
-  set setInput(SMIBool status) {
-    input = status;
-  }
-}
-
-List<RiveAsset> bottomNavs = [
-  RiveAsset("assets/RiveAssets/icons.riv",
-      artboard: "HOME", stateMachineName: "HOME_interactivity", title: "Home"),
-  RiveAsset("assets/RiveAssets/icons.riv",
-      artboard: "TIMER",
-      stateMachineName: "TIMER_Interactivity",
-      title: "Timer"),
-  RiveAsset("assets/RiveAssets/icons.riv",
-      artboard: "BELL",
-      stateMachineName: "BELL_Interactivity",
-      title: "Notification"),
-  RiveAsset("assets/RiveAssets/icons.riv",
-      artboard: "USER",
-      stateMachineName: "USER_Interactivity",
-      title: "Notification"),
-];
