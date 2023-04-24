06e5067b28bc78aa963130066e60859bab48ee6d
Maxtro2002 <jspr200231@gmail.com>
2023-04-24 18:45:24 -0500

    Mejora estilo placas

--- lib/screens/home/homeScreen.dart
+++ lib/screens/home/homeScreen.dart
@@ -28,62 +28,79 @@ class HomeScreen extends StatelessWidget {
                     ...List.generate(
                       userVehicles.length,
                       (index) => Container(
-                        padding: const EdgeInsets.all(20),
+                        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
                         child: Container(
-                          height: 200,
-                          width: double.infinity,
-                          decoration: const BoxDecoration(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 2,
+                            ),
                             color: Colors.amberAccent,
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
                           ),
-                          child: Center(
-                            child: Padding(
-                              padding: const EdgeInsets.all(12),
-                              child: RichText(
-                                text: TextSpan(
-                                  text: "Placa:",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headlineMedium!
-                                      .copyWith(
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.w600,
-                                          fontSize: 20),
-                                  children: [
-                                    TextSpan(
-                                      text:
-                                          "${userVehicles[index].carPlateLetter}-${userVehicles[index].carPlateNumbers}",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headlineMedium!
-                                          .copyWith(
-                                              color: Colors.black,
-                                              fontWeight: FontWeight.w600,
-                                              fontSize: 30),
-                                    ),
-                                    TextSpan(
-                                      text:
-                                          "\nPropietario: ${userVehicles[index].propetary}",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headlineMedium!
-                                          .copyWith(
-                                              color: Colors.black26,
-                                              fontWeight: FontWeight.normal,
-                                              fontSize: 15),
-                                    ),
-                                    TextSpan(
-                                      text:
-                                          "\nTipo de vehiculo: ${userVehicles[index].vehicleTyper}",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headlineMedium!
-                                          .copyWith(
-                                              color: Colors.black26,
-                                              fontWeight: FontWeight.normal,
-                                              fontSize: 15),
-                                    )
-                                  ],
+                          padding: const EdgeInsets.all(12),
+                          child: Container(
+                            height: 200,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 8,
+                              ),
+                              color: Colors.amberAccent,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(20)),
+                            ),
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: RichText(
+                                  text: TextSpan(
+                                    text: "Placa:",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineMedium!
+                                        .copyWith(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 20),
+                                    children: [
+                                      TextSpan(
+                                        text:
+                                            "${userVehicles[index].carPlateLetter}-${userVehicles[index].carPlateNumbers}",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headlineMedium!
+                                            .copyWith(
+                                                color: Colors.black,
+                                                fontWeight: FontWeight.w600,
+                                                fontSize: 30),
+                                      ),
+                                      TextSpan(
+                                        text:
+                                            "\nPropietario: ${userVehicles[index].propetary}",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headlineMedium!
+                                            .copyWith(
+                                                color: Colors.black26,
+                                                fontWeight: FontWeight.normal,
+                                                fontSize: 15),
+                                      ),
+                                      TextSpan(
+                                        text:
+                                            "\nTipo de vehiculo: ${userVehicles[index].vehicleTyper}",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headlineMedium!
+                                            .copyWith(
+                                                color: Colors.black26,
+                                                fontWeight: FontWeight.normal,
+                                                fontSize: 15),
+                                      )
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
