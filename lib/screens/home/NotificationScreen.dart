86bc7e5a33b0059d8a1466b0c52de7ad607f10e9
Maxtro2002 <jspr200231@gmail.com>
2023-04-25 21:30:15 -0500

    Pagina notificaciones

--- lib/screens/home/NotificationScreen.dart
+++ lib/screens/home/NotificationScreen.dart
@@ -1,12 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:rive_animation/models/notifications.dart';
 
 class NotificationsScreen extends StatelessWidget {
   const NotificationsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Notificacion", style: TextStyle(fontSize: 30),),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "Notificaciones",
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineMedium!
+                    .copyWith(color: Colors.black, fontWeight: FontWeight.w600),
+              ),
+            ),
+            Expanded(
+                child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  ...List.generate(
+                    userNotifications.length,
+                    (index) => Container(
+                      padding: EdgeInsets.all(12),
+                      child: Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                          color: Colors.lightGreen.shade200,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(20)),
+                        ),
+                        child: Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(12),
+                            child: RichText(
+                              text: TextSpan(
+                                text: "${userNotifications[index].title}: ",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headlineMedium!
+                                    .copyWith(
+                                        color: Colors.green,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 20),
+                                children: [
+                                  TextSpan(
+                                    text: userNotifications[index].desciption,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineMedium!
+                                        .copyWith(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 20),
+                                  ),
+                                  TextSpan(
+                                      text:
+                                          "\n\nFecha limite de pago: ${userNotifications[index].limitDate}",
+                                          style: Theme.of(context)
+                                        .textTheme
+                                        .headlineMedium!
+                                        .copyWith(
+                                            color: Colors.red,
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 10),),
+                                  TextSpan(
+                                      text:
+                                          "\nFecha de pago anterior: ${userNotifications[index].limitDate}",
+                                          style: Theme.of(context)
+                                        .textTheme
+                                        .headlineMedium!
+                                        .copyWith(
+                                            color: Colors.grey,
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 10),),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )),
+          ],
+        ),
+      ),
     );
   }
 }
