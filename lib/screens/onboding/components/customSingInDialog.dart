5feb06c0b008304b7701741dd1bd108bc553965b
Maxtro2002 <jspr200231@gmail.com>
2023-05-28 14:08:14 -0500

    Iconos verificacion agregados

--- lib/screens/onboding/components/customSingInDialog.dart
+++ lib/screens/onboding/components/customSingInDialog.dart
@@ -1,67 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rive_animation/screens/onboding/components/singInForm.dart';
 
-Future<Object?> CustomSingInDialog(BuildContext context, {required ValueChanged onClosed}) {
-    return showGeneralDialog(
-      barrierDismissible: true,
-      barrierLabel: "Ingresar",
-      context: context,
-      transitionDuration: const Duration(milliseconds: 400),
-      transitionBuilder: (_, animation, __, child) {
-        Tween<Offset> tween;
-        tween = Tween<Offset>(
-          begin: const Offset(0, 1),
-          end: Offset.zero,
-        );
-        return SlideTransition(
-          position: tween.animate(
-            CurvedAnimation(parent: animation, curve: Curves.easeInOut),
+Future<Object?> CustomSingInDialog(BuildContext context,
+    {required ValueChanged onClosed}) {
+  return showGeneralDialog(
+    barrierDismissible: true,
+    barrierLabel: "Ingresar",
+    context: context,
+    transitionDuration: const Duration(milliseconds: 400),
+    transitionBuilder: (_, animation, __, child) {
+      Tween<Offset> tween;
+      tween = Tween<Offset>(
+        begin: const Offset(0, 1),
+        end: Offset.zero,
+      );
+      return SlideTransition(
+        position: tween.animate(
+          CurvedAnimation(parent: animation, curve: Curves.easeInOut),
+        ),
+        child: child,
+      );
+    },
+    pageBuilder: (context, _, __) => Center(
+      child: Container(
+        height: 620,
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 32,
+          vertical: 24,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.94),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(40),
           ),
-          child: child,
-        );
-      },
-      pageBuilder: (context, _, __) => Center(
-        child: Container(
-          height: 620,
-          margin: const EdgeInsets.symmetric(horizontal: 16),
-          padding: const EdgeInsets.symmetric(
-            horizontal: 32,
-            vertical: 24,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.94),
-            borderRadius: const BorderRadius.all(
-              Radius.circular(40),
-            ),
-          ),
-          child: const Scaffold(
-            backgroundColor: Colors.transparent,
-            body: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      "Ingresar",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontFamily: "Poppins",
-                      ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Column(
+                children: [
+                  const Text(
+                    "Ingresar",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontFamily: "Poppins",
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(16),
-                      child: Text(
-                        "Ingresa a tu cuenta para poder gestionar tus horarios y planificar tus salidas",
-                        textAlign: TextAlign.center,
-                      ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      "Ingresa a tu cuenta para poder gestionar tus horarios y planificar tus salidas",
+                      textAlign: TextAlign.center,
                     ),
-                    SingInForm(),
-                  ],
+                  ),
+                  const SingInForm(),
+                  const Row(
+                    children: [
+                      Expanded(child: Divider()),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(
+                          "O",
+                          style: TextStyle(
+                            color: Colors.black26,
+                          ),
+                        ),
+                      ),
+                      Expanded(child: Divider()),
+                    ],
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 24),
+                    child: Text(
+                      "Registrarse con Google O Github",
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/icons/google.svg",
+                          height: 64,
+                          width: 64,
+                        ),
+                      ),
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/icons/githuub.svg",
+                          height: 64,
+                          width: 64,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const Positioned(
+                left: 0,
+                right: 0,
+                bottom: -48,
+                child: CircleAvatar(
+                  radius: 16,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.close,
+                    color: Colors.black,
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
-    ).then(onClosed);
-  }
+    ),
+  ).then(onClosed);
+}
